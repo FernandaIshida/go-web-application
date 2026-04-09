573ec8319316a2fe630c21606455a26ae56b1524
Fernanda Ishida <fer.ishida@gmail.com>
2026-04-09 18:10:55 -0300

    feat: add insert handler for products

--- controllers/products.go
+++ controllers/products.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/FernandaIshida/go-web-application/models"
 )
@@ -12,4 +14,31 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	allProducts := models.GetAllProducts()
 	temp.ExecuteTemplate(w, "Index", allProducts)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+
+		name := r.FormValue("name")
+		description := r.FormValue("description")
+		price := r.FormValue("price")
+		quantity := r.FormValue("quantity")
+
+		convertPriceToFloat, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			log.Println("Error converting price:", err)
+		}
+
+		convertQuantityToInt, err := strconv.Atoi(quantity)
+		if err != nil {
+			log.Println("Error converting quantity:", err)
+		}
+
+		models.CreateNewProduct(name, description, convertPriceToFloat, convertQuantityToInt)
+	}
+	http.Redirect(w, r, "/", 301)
 }
