5132237b964d4587c734559f13948650511f4c2c
Fernanda Ishida <fer.ishida@gmail.com>
2026-04-09 18:09:32 -0300

    feat: implement product creation in model

--- models/products.go
+++ models/products.go
@@ -47,3 +47,18 @@ func GetAllProducts() []Product {
 	defer db.Close()
 	return products
 }
+
+func CreateNewProduct(name, description string, price float64, quantity int) {
+	db, err := db.DataBaseConnection()
+	if err != nil {
+		log.Fatal("Database connection error:", err)
+	}
+
+	insertDataInBase, err := db.Prepare("INSERT INTO products(name, description, price, quantity) VALUES($1, $2, $3, $4)")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	insertDataInBase.Exec(name, description, price, quantity)
+	defer db.Close()
+}
